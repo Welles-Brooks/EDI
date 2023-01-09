eba3c416ed153e8dee1a8499d0494bf7317d1f40
Welles-Brooks <113536917+Welles-Brooks@users.noreply.github.com>
2023-01-09 09:54:20 -0500

    h

---
+++ CreativeCoding/p5/Grid_Gif/sketch.js
@@ -0,0 +1,20 @@
+function setup() {
+  createCanvas(windowWidth,windowWidth);
+  background(255,255,0);
+}
+
+function draw() {
+  fill(0,255,255);
+  strokeWeight(1);
+  var num = 2;
+  var sideLen = windowWidth/num;
+for (var y = 0; y <windowWidth;y += 360){
+  for(var x = 0; x<windowWidth;x += 360){
+    quad(x,y,
+      x + sideLen,y,
+      x +sideLen,y+ sideLen,
+      x, y + sideLen);
+  }
+}
+
+}
