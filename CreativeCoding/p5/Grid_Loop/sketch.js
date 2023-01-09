137c34e22aa6f61a67bebf58be6911dd9b6ce978
Welles-Brooks <113536917+Welles-Brooks@users.noreply.github.com>
2023-01-09 09:25:36 -0500

    k

---
+++ CreativeCoding/p5/Grid_Loop/sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(720,720);
+  background(255,255,0);
+}
+
+function draw() {
+  translate(60,60);
+  fill(255,0,0);
+  strokeWeight(8);
+  stroke(0,0,255)
+  quad(0,0,300,0,300,300,0,300)
+  quad(300,0,600,0,600,300,300,300)
+  quad(0,300,300,300,300,600,0,600)
+  quad(300,3000,600,300,600,600,300,600)
+}
