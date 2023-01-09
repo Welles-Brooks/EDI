5c9e40861c683512fcd895eda24ff090b2f3b8a9
Welles-Brooks <113536917+Welles-Brooks@users.noreply.github.com>
2023-01-09 10:07:46 -0500

    h

--- CreativeCoding/p5/Grid_Loop/sketch.js
+++ CreativeCoding/p5/Grid_Loop/sketch.js
@@ -6,10 +6,10 @@ function setup() {
 function draw() {
   fill(0,255,255);
   strokeWeight(1);
-  var num = 2;
+  var num = 4;
   var sideLen = windowWidth/num;
-for (var y = 0; y <windowWidth;y += 360){
-  for(var x = 0; x<windowWidth;x += 360){
+for (var y = 0; y <windowWidth;y += sideLen){
+  for(var x = 0; x<windowWidth;x += sideLen){
     quad(x,y,
       x + sideLen,y,
       x +sideLen,y+ sideLen,
