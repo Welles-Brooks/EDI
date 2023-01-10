5ffb05759eb015e4bc882166a9a8c743e21e3e1a
Welles-Brooks <113536917+Welles-Brooks@users.noreply.github.com>
2023-01-10 12:53:22 -0500

    h

---
+++ sketch copy.js
@@ -0,0 +1,31 @@
+ let gif;
+
+ function preload(){
+
+   gif = loadImage ('assets/Dancing_Yoshi.gif');
+ }
+
+function setup() {
+  createCanvas(windowWidth,windowWidth);
+  background(255,255,0);
+}
+
+function draw() {
+  fill(0,255,255);
+  strokeWeight(1);
+  var num = 4;
+  var sideLen = windowWidth/num;
+
+  translate(-150,-150);//give design bleed effect
+
+for (var y = 0; y < 2 * windowWidth;y += sideLen){
+  for(var x = 0; x< 2 * windowWidth;x += sideLen){
+      image (gif,x,y,sideLen,sideLen);
+  }
+}
+
+}
+
+function windowResized (){// This resize and refreshes the design with broswer
+  resizeCanvas (windowWidth,windowHeight);
+}
